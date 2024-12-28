26fac859fbcdae084a7e6b4a5654180960f56fdc
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2024-12-28 12:28:46 +0530

    ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import UserLogin from './pages/UserLogin'
+import UserSignUp from './pages/UserSignUp'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+   <>
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<UserLogin />} />
+    <Route path="/signup" element={<UserSignUp/>} />
+    <Route path="/captain-login" element={<CaptainLogin/>} />
+    <Route path="/captain-signup" element={<CaptainSignup/>} />
+   </Routes>
+   </>
   )
 }
 
